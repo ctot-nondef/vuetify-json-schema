bc73c2e181a634a8d8bb25c493d9479eae222455
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2018-10-04 18:38:45 +0200

    moved webpackconfig+build

--- build/build.js
+++ build/build.js
@@ -7,7 +7,7 @@ const rm = require('rimraf')
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
-const webpackConfig = require('./webpack.config')
+const webpackConfig = require('./build/webpack.config')
 const main = require('./package').main
 
 const spinner = ora('building for production...')
