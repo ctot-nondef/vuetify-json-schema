33de5ca4596c31e141abd05f18ee29dbb4128ab9
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2018-10-04 19:58:57 +0200

    added dev context

---
+++ src/components/app.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <formschema />
+  </div>
+</template>
+
+<script>
+import FormSchema from './components/FormSchema';
+
+export default {
+  name: 'App',
+  components: {
+    FormSchema,
+  },
+};
+</script>
+
+<style lang="scss">
+  #app {
+    padding: 16px;
+    max-width: 1400px;
+    margin: auto;
+  }
+
+  .flex {
+    display: flex;
+  }
+
+  .row {
+    max-height: 300px;
+  }
+
+  div > .base-box-button {
+    margin: 8px;
+  }
+
+  button {
+    display:block;
+  }
+
+  .popup-text {
+    display: flex;
+    align-items: flex-end;
+  }
+
+  .form-field {
+    background-color: white;
+    padding: 16px;
+    margin-bottom: 32px;
+  }
+</style>
